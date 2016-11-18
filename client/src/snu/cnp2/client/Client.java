930b8f4b88efd2b78e21532802d375d17a882c6b
noris7012 <noris7012@gmail.com>
2016-11-18 21:34:01 +0900

    [Client] Download File

--- client/src/snu/cnp2/client/Client.java
+++ client/src/snu/cnp2/client/Client.java
@@ -3,11 +3,15 @@ package snu.cnp2.client;
 import com.coremedia.iso.IsoFile;
 import com.googlecode.mp4parser.boxes.apple.AppleNameBox;
 import com.googlecode.mp4parser.util.Path;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by noris on 2016-11-18.
@@ -15,8 +19,9 @@ import java.io.IOException;
 public class Client {
     public static void main(String[] args) throws IOException {
         Client cmd = new Client();
-        String xml = cmd.read("resource\\BigBuckBunny_4s3.m4s");
-        System.err.println(xml);
+//        String xml = cmd.read("resource\\BigBuckBunny_4s3.m4s");
+//        System.err.println(xml);
+        cmd.downloadFile();
     }
 
     public String read(String videoFilePath) throws IOException {
@@ -26,5 +31,30 @@ public class Client {
         String xml = nam.getValue();
         isoFile.close();
         return xml;
+    }
+
+    public void downloadFile() {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            String url = "http://cn.snucse.org/1473801/BigBuckBunny_4s3.m4s";
+            HttpGet get = new HttpGet(url);
+
+            HttpResponse response = client.execute(get);
+
+            // TODO Entity Null Check
+            HttpEntity entity = response.getEntity();
+
+            String filePath = "sample.txt";
+
+            try (BufferedInputStream bis = new BufferedInputStream(entity.getContent());
+                 BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(filePath)))) {
+                int inByte;
+                while((inByte = bis.read()) != -1)
+                    bos.write(inByte);
+            } catch (IOException e) {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
